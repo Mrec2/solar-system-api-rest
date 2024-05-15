12bd6f3c9821b488c8886c6529e7af55b5de03bd
Mario Fernandez <marioteleco@outlook.com>
2024-05-15 20:16:55 +0200

    first commit

---
+++ src/main/java/org/mario/springboot/solarsystemapirest/SolarSystemApirestApplication.java
@@ -0,0 +1,13 @@
+package org.mario.springboot.solarsystemapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarSystemApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolarSystemApirestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mario/springboot/solarsystemapirest/SolarSystemApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mario.springboot.solarsystemapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarSystemApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
