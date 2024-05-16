f3d337e62a75ab03a524cdadc80cf3362450860c
Mario Fernandez <marioteleco@outlook.com>
2024-05-16 12:59:58 +0200

    Service was added as service and used DI from repositories

--- src/main/java/org/mario/springboot/solarsystemapirest/services/PlanetServiceImpl.java
+++ src/main/java/org/mario/springboot/solarsystemapirest/services/PlanetServiceImpl.java
@@ -1,19 +1,25 @@
 package org.mario.springboot.solarsystemapirest.services;
 
 import org.mario.springboot.solarsystemapirest.models.Planet;
+import org.mario.springboot.solarsystemapirest.repositories.PlanetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class PlanetServiceImpl implements PlanetService{
 
+    @Autowired
+    PlanetRepository repository;
 
     @Override
     public List<Planet> getPlanets() {
-        return List.of();
+        return repository.getPlanets();
     }
 
     @Override
     public Planet findPlanetById(Long id) {
-        return null;
+        return repository.findPlanetById(id);
     }
 }
